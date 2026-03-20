52e4c465ddc42107511351d392481fd0bb1a4624
znavidi <z.navidi90@gmail.com>
2026-03-20 15:14:33 -0400

    Fixed an issue in scvi train

--- bash/train_scvi_for_different_k_and_epoch.sh
+++ bash/train_scvi_for_different_k_and_epoch.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+echo "Starting the script"
 # Assign input parameters to variables
 data=sctab            # data base name, e.g., sctab
 data_path=./data       # path to the /dataset/ folder
 out_path=./saved_models        # path to the folder for saving trained models (ends with /saved_models/)
-seed=(50 60 70 80 90)            # seed number
+seeds=(50 60 70 80 90)            # seed number
 log_path=./log        # path to save logs
 root=../            # root folder of the project
 model_name=scvi      # model type: scvi or scgen
 latent_dim=64      # latent dimension
-num_epoch=(200 500)       # number of epochs
-AR=(True False)           # AR flag: True or False
-atlas_count=(0 1 10 100 1000 10000 50000)  # number of atlas cells, e.g., 0, 1, 10, 100, 1000, 10000, 50000
+num_epochs=(200 500)       # number of epochs
+ARs=(True False)           # AR flag: True or False
+atlas_counts=(0 1 10 100 1000 10000 50000)  # number of atlas cells, e.g., 0, 1, 10, 100, 1000, 10000, 50000
 alpha=0.0001        # alpha value for smoothing
 
 
@@ -39,49 +39,50 @@ batch_size=4096
 ############################
 # Loop
 ############################
+echo "Starting the loop"
 for seed in "${seeds[@]}"; do
-  for num_epoch in "${num_epochs[@]}"; do
-    for AR in "${ARs[@]}"; do
-      for atlas_count in "${atlas_counts[@]}"; do
-        echo "Running scvi for seed ${seed}, num_epoch ${num_epoch}, AR ${AR}, atlas_count ${atlas_count}"
+    for num_epoch in "${num_epochs[@]}"; do
+        for AR in "${ARs[@]}"; do
+            for atlas_count in "${atlas_counts[@]}"; do
+              echo "Running scvi for seed ${seed}, num_epoch ${num_epoch}, AR ${AR}, atlas_count ${atlas_count}"
 
-        echo "=============================================="
-        echo "Running:"
-        echo "  seed=${seed}"
-        echo "  num_epoch=${num_epoch}"
-        echo "  latent_dim=${latent_dim}"
-        echo "  AR=${AR}"
-        echo "  atlas_count=${atlas_count}"
-        echo "=============================================="
+              echo "=============================================="
+              echo "Running:"
+              echo "  seed=${seed}"
+              echo "  num_epoch=${num_epoch}"
+              echo "  latent_dim=${latent_dim}"
+              echo "  AR=${AR}"
+              echo "  atlas_count=${atlas_count}"
+              echo "=============================================="
 
-        CUDA_VISIBLE_DEVICES=0 python -u ${root}/main.py \
-            --root ${root} \
-            --data ${data} \
-            --data_path ${data_path} \
-            --out_path ${out_path} \
-            --train True \
-            --test False \
-            --predict False \
-            --latent_dim ${latent_dim} \
-            --AR ${AR} \
-            --ood ${ood} \
-            --num_epoch ${num_epoch} \
-            --seed ${seed} \
-            --model_name "scvi" \
-            --atlas_count ${atlas_count} \
-            --batch_size ${batch_size} \
-            --alpha ${alpha} >> ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.txt 2> ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err 
+              CUDA_VISIBLE_DEVICES=0 python -u ${root}/main.py \
+                  --root ${root} \
+                  --data ${data} \
+                  --data_path ${data_path} \
+                  --out_path ${out_path} \
+                  --train True \
+                  --test False \
+                  --predict False \
+                  --latent_dim ${latent_dim} \
+                  --AR ${AR} \
+                  --ood ${ood} \
+                  --num_epoch ${num_epoch} \
+                  --seed ${seed} \
+                  --model_name "scvi" \
+                  --atlas_count ${atlas_count} \
+                  --batch_size ${batch_size} \
+                  --alpha ${alpha} >> ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.txt 2> ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err 
 
-        # Error checking
-        if grep -q "Traceback" ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err; then
-            echo "Error"
-        fi
+              # Error checking
+              if grep -q "Traceback" ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err; then
+                  echo "Error"
+              fi
 
-        if grep -q "main.py: error:" ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err; then
-            echo "Error"
-        fi
+              if grep -q "main.py: error:" ${log_path}/$today/${today}-${data}-AR${AR}-ood${ood}-seed${seed}-epoch${num_epoch}-ldim${latent_dim}-${model_name}-${atlas_count}atlas-alpha${alpha}-bs${batch_size}-train.err; then
+                  echo "Error"
+              fi
 
-      done
+            done
+        done
     done
-  done
 done
