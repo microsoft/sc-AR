9ff07d1ffca278025083b8eefc7cbf17dcfb46a9
znavidi <z.navidi90@gmail.com>
2026-04-20 10:53:52 -0400

    Editted scvi training script for Slurm job.

--- bash/train_scvi_for_different_k_and_epoch.sh
+++ bash/train_scvi_for_different_k_and_epoch.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
-echo "Starting the script"
+#SBATCH -n 1
+#SBATCH --mem=120G
+#SBATCH -t 1:00:00
+#SBATCH -c 23
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH -J gpu_train_scAR
+#SBATCH -o output/%x_%j.out    
+#SBATCH -e error/%x_%j.err    
+
+# Email notifications
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=akshaya_thoutam@brown.edu
+
+# Unload all modules (important)
+module purge
+
+## Please check the version of cuda and cudnn
+module load cuda/11.6.0-wmwrm7d
+module load cudnn/8.7.0.84-11.8-lg2dpd5
+
+# activate the conda environment
+eval "$(conda shell.bash hook)"
+source activate scAR-env
+export WANDB_KEY=''
+
+# Debug: confirm which python is being used
+which python
+python -c "import sys; print('Python path:', sys.executable); import scanpy; print('Scanpy OK')"
+python -c "import torch; print(torch.__version__, torch.cuda.is_available(), torch.version.cuda)"
+
+
+
 # Assign input parameters to variables
 data=sctab            # data base name, e.g., sctab
 data_path=./data       # path to the /dataset/ folder
@@ -19,11 +51,6 @@ latent_dim_epoch_pairs=(
     (32 300)
     (128 300)
 )
-
-# activate the conda environment
-eval "$(conda shell.bash hook)"
-source activate scAR-env
-export WANDB_KEY=''
 
 # create log folder
 today=$(date '+%Y%m%d')
